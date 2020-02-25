886f3bab15da8336a14d92540a27d86b10179f95
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-02-25 15:03:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,10 +38,10 @@ hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[key].included? 
-    has[key] += 1 
+  if hash[key]
+    has[key] = 1 
   else 
-    hash[key] = 1
+    hash[key] += 1
   end
   
   # given a hash an a key as parameters, return an updated hash
